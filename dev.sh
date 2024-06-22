cf8686e63e3f5bc15f595b735d90cf6585a36e12
Kacper Paczos <kacper.paczos@linux.pl>
2024-06-22 18:02:16 +0200

    I added 10MiB limit. From now on, the steps are performed one by one in succession.

--- dev.sh
+++ dev.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# Remove builddir directory, configure and compile the project
-rm -rf builddir
-meson setup builddir
-meson compile -C builddir 
-
+# Remove the builddir directory, configure and compile the project
+rm -rf builddir && \
+meson setup builddir && \
+meson compile -C builddir && \
+# Set memory limit to 10MiB
+ulimit -v 10240 && \
 # Run the compiled program with root privileges
 sudo ./builddir/src/ethernetcap
